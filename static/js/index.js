e3da52fe35e5305eb1ed8ac6870fb8992debbc32
ilmar <ilmar.tyrk@gmail.com>
2020-12-01 13:00:15 +0200

    Update code, add eslint

--- static/js/index.js
+++ static/js/index.js
@@ -1,75 +1,83 @@
 'use strict';
 
-var lastHeight;
-var lastWidth;
-var returnchildHeights = function (children, offsetTop) {
-    offsetTop = offsetTop + 10 || 10; //Some extra padding for possible shadows etc.
+let lastHeight;
+let lastWidth;
+const returnchildHeights = (children, offsetTop) => {
+  offsetTop = offsetTop + 10 || 10; // Some extra padding for possible shadows etc.
 
-    var maxHeight = 0;
+  let maxHeight = 0;
 
-    if (children.length) {
-        maxHeight = 0;
-        children.each(function (key, child) {
-            if ($(child).is(':visible') && $(child).attr('id') !== 'editorcontainerbox' && $(child).attr('id') !== 'editbar' && !$(child).is('iframe') && $(child)[0].offsetHeight > 0) { // Avoid infinit increasing
-                var childtop = ($(child).offset().top + $(child)[0].offsetHeight) + offsetTop;
-                if (childtop > maxHeight) {
-                    maxHeight = childtop;
-                }
-            }
-        });
-    }
-
-    return maxHeight;
-};
-
-exports.aceEditEvent = function (event, context, callback) {
-    var ace_outer_top = $('iframe[name=ace_outer]').offset().top;
-    var ace_inner_top = $('iframe[name=ace_outer]').contents().find('iframe[name=ace_inner]').offset().top;
-    var getFinalLine = context.rep.lines.atIndex(context.rep.lines.length()-1);
-    var finalLine =  $(getFinalLine.lineNode);
-
-    var elem = $('iframe[name=ace_outer]').contents().find('iframe[name=ace_inner]');
-    var newHeight = finalLine.offset().top + finalLine.outerHeight() + ace_outer_top + ace_inner_top;
-    var newWidth = elem.outerWidth();
-
-    var maxChild = returnchildHeights($('iframe[name=ace_outer]').contents().find('body').children(), ace_outer_top);
-    var maxChildBody = returnchildHeights($('body').children());
-
-    if (maxChildBody > maxChild) {
-        maxChild = maxChildBody
-    }
-
-    if (maxChild > newHeight) {
-        newHeight = maxChild;
-    }
-    if (!lastHeight || !lastWidth || lastHeight !== newHeight || lastWidth !== newWidth) {
-        if (newHeight - lastHeight !== 10)
-            sendResizeMessage(newWidth, newHeight);
-    }
-
-};
-
-exports.goToRevisionEvent = function (hook_name, context, cb) {
-
-    var editbar = $('#editbar')
-    var elem = $('#outerdocbody');
-    var newHeight = elem.outerHeight() + (editbar.length ? editbar.outerHeight() : 0);
-    var newWidth = elem.outerWidth();
-
-    if (!lastHeight || !lastWidth || lastHeight !== newHeight || lastWidth !== newWidth) {
-        sendResizeMessage(newWidth, newHeight);
-    }
-};
-
-var sendResizeMessage = function (width, height) {
-    lastHeight = height;
-    lastWidth = width;
-
-    window.parent.postMessage({
-        name: 'ep_resize',
-        data: {
-            width:  width,
-            height: height
+  if (children.length) {
+    maxHeight = 0;
+    children.each((key, child) => {
+      const cid = $(child).attr('id');
+      const isIframe = $(child).is('iframe');
+      const isVisible = $(child).is(':visible');
+      const validElem = ['editorcontainerbox', 'editbar'].indexOf(cid) === -1;
+      const hasHeight = $(child)[0].offsetHeight > 0;
+      // Avoid infinit increasing
+      if (isVisible && validElem && !isIframe && hasHeight) {
+        const childtop = ($(child).offset().top + $(child)[0].offsetHeight) + offsetTop;
+        if (childtop > maxHeight) {
+          maxHeight = childtop;
         }
-    }, '*');
-}
+      }
+    });
+  }
+
+  return maxHeight;
+};
+
+const sendResizeMessage = (width, height) => {
+  lastHeight = height;
+  lastWidth = width;
+
+  window.parent.postMessage({
+    name: 'ep_resize',
+    data: {
+      width,
+      height,
+    },
+  }, '*');
+};
+
+exports.aceEditEvent = (event, context, cb) => {
+  const padOuter = $('iframe[name=ace_outer]');
+  const ace_outer_top = padOuter.offset().top;
+  const ace_inner_top = padOuter.contents().find('iframe[name=ace_inner]').offset().top;
+  const getFinalLine = context.rep.lines.atIndex(context.rep.lines.length() - 1);
+  const finalLine = $(getFinalLine.lineNode);
+
+  const elem = $('iframe[name=ace_outer]').contents().find('iframe[name=ace_inner]');
+  let newHeight = finalLine.offset().top + finalLine.outerHeight() + ace_outer_top + ace_inner_top;
+  const newWidth = elem.outerWidth();
+
+  let maxChild = returnchildHeights(padOuter.contents().find('body').children(), ace_outer_top);
+  const maxChildBody = returnchildHeights($('body').children());
+
+  if (maxChildBody > maxChild) {
+    maxChild = maxChildBody;
+  }
+
+  if (maxChild > newHeight) {
+    newHeight = maxChild;
+  }
+  if (!lastHeight || !lastWidth || lastHeight !== newHeight || lastWidth !== newWidth) {
+    if (newHeight - lastHeight !== 10) sendResizeMessage(newWidth, newHeight);
+  }
+
+  return cb();
+};
+
+exports.goToRevisionEvent = (hook, context, cb) => {
+  const editbar = $('#editbar');
+  const elem = $('#outerdocbody');
+  const newHeight = elem.outerHeight() + (editbar.length ? editbar.outerHeight() : 0);
+  const newWidth = elem.outerWidth();
+
+  if (!lastHeight || !lastWidth || lastHeight !== newHeight || lastWidth !== newWidth) {
+    sendResizeMessage(newWidth, newHeight);
+  }
+
+  return cb();
+};
